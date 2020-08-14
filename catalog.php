138f1a9f90f408194f0067a8d00b0f473c59e61e
TimaPaps <ptv.devstudio@gmail.com>
2020-08-14 11:19:27 +0500

    add files

---
+++ catalog.php
@@ -0,0 +1,7 @@
+<!--подключение файла стилей-->
+<link rel="stylesheet" href="/css/style.css">
+
+<div id="catalog"></div>
+
+<!--подключение файла javascript-->
+<script src="/js/script.js"></script>
