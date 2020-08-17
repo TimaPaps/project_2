2639b58cffa592b3a138278356faaf0e2b351728
TimaPaps <ptv.devstudio@gmail.com>
2020-08-17 11:28:35 +0500

    верстка главной страницы index.php

--- catalog.php
+++ catalog.php
@@ -3,7 +3,23 @@
     include($_SERVER['DOCUMENT_ROOT'] . '/inc/header.php');
 ?>
 
+<div class="wrapper">ГЛАВНАЯ / МУЖЧИНАМ</div>
+<div class="wrapper text-align-center">
+    <h1>МУЖЧИНАМ</h1>
+    <p>Все товары</p>
+    <div class="flex-box justify-content-center">
+        <div class="padding-10">Категория</div>
+        <div class="padding-10">Размер</div>
+        <div class="padding-10">Стоимость</div>
+    </div>
+</div>
 <div id="catalog" class="wrapper"></div>
+<div class="flex-box justify-content-center">
+    <div class="padding-10">1</div>
+    <div class="padding-10">2</div>
+    <div class="padding-10">3</div>
+    <div class="padding-10">4</div>
+</div>
 
 <?php
     include($_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php');
--- index.php
+++ index.php
@@ -3,8 +3,43 @@
     include($_SERVER['DOCUMENT_ROOT'] . '/inc/header.php');
 ?>
 <body>
-    <main>
-    
+    <main class="wrapper">
+        <div class="text-align-center margin-top-60">
+            <h1>НОВЫЕ ПОСТУПЛЕНИЯ ВЕСНЫ</h1>
+            <p>Мы подготовили для Вас лучшие новинки сезона</p>
+            <p class="btn-10-30 margin-top-40">посмотреть новинки</p>
+        </div>
+        <div class="flex-box margin-top-30">
+            <div class="main-item">
+                <div class="main-photo-1 main-photo-rectangle"></div>
+                <div class="main-photo-5 main-photo-square"></div>
+            </div>
+            <div class="main-item">
+                <div class="main-background-light-gray main-photo-square"></div>
+                <div class="main-photo-2 main-photo-square"></div>
+                <div class="main-background-gray main-photo-square"></div>
+            </div>
+            <div class="main-item">
+                <div class="main-photo-3 main-photo-square"></div>
+                <div class="main-background-light-gray main-photo-square"></div>
+                <div class="main-photo-6 main-photo-square"></div>
+            </div>
+            <div class="main-item">
+                <div class="main-background-gray main-photo-square"></div>
+                <div class="main-photo-4 main-photo-rectangle"></div>
+            </div>
+        </div>
+        <div class="text-align-center margin-top-100">
+            <h2>БУДЬ ВСЕГДА В КУРСЕ ВЫГОДНЫХ ПРЕДЛОЖЕНИЙ</h2>
+            <p>подписывайся и следи за новинками и выгодными предложениями.</p>
+        </div>
+        <div class="flex-box justify-content-center margin-top-40">
+            <form action="" method="POST" class="border-1px">
+                <input type="email" name="email" id="" placeholder="e-mail">
+                <input type="submit" value="записаться">
+            </form>
+        </div>
+        <p class="text-align-center">Некорректный e-mail. Попробуйте еще раз.</p>
     </main>
 <?php
     include($_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php');
