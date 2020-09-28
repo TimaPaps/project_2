f1ecb08833c544dbe6b41f48cd957fc41f32ce69
TimaPaps <ptv.devstudio@gmail.com>
2020-09-28 23:23:51 +0500

    криптование пароля пользователя при изменении из админ панели, простой вывод яндекс карты и точек на карте

--- system/controllers/users/update.php
+++ system/controllers/users/update.php
@@ -6,6 +6,11 @@ session_start();
 //var_dump($_FILES);
 
 $id = (int)$_POST['id'];
+/*
+var_dump($id);
+$pass = $_POST['password'];
+var_dump($pass);
+*/
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config/db_config.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/system/classes/autoload.php');
@@ -17,14 +22,33 @@ $arr_values = [];
 
 //разбор всех пришедших данных
 foreach ($_POST as $key => $value) {
+/*  //вариант Тимофей
+    if ($key != 'id') {
+        $arr_fields[] = $key;       
+   
+        if ($key == 'password') {
+            $arr_values[] = "'" . crypt($value) . "'";
+            var_dump($arr_values);
+        } else {
+            $arr_values[] = "'" . $value . "'";
+            var_dump($arr_values);
+        }
+    } 
+    */
+    //мой вариант
     if ($key != 'id') {
         $arr_fields[] = $key;
-        $arr_values[] = "'" . $value . "'";        
-    }
+        $arr_values[] = "'" . $value . "'";  
+        //var_dump($arr_values);
+    } 
+    if ($key == 'password') {
+        $arr_values[count($arr_values)-1] = "'" . crypt($value) . "'";
+        //var_dump($arr_values);
+    }    
 }
 
 //var_dump($arr_fields);
-var_dump($arr_values);
+//var_dump($arr_values);
 
 //сборка строки для подстановки в запрос
 $str_update = '';
@@ -42,9 +66,10 @@ $connect = new \Project\Core\Connect();
 echo "UPDATE core_users SET $str_update WHERE id=$id  ";
 
 $result = mysqli_query($connect->getConnection(), "UPDATE core_users SET $str_update WHERE id=$id "); //при автоматическом получении данных полей формы
+var_dump($result);
 
 if ($result) {
-    //echo 'Товар создан';
+    //echo 'Клиент изменен';
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 } else {
     echo 'что то пошло не так';
